8edbdf22411ce89a420d1d7fe8cd0f3a902506d8
G_Polya <wlskaka4@gmail.com>
2020-11-17 15:09:39 +0900

    db에서 데이터불러오기(건우님방법)으로 성공

--- KimHeeSu_workspace/teamFile/myHTML/mongoDBtest.js
+++
@@ -1,6 +0,0 @@
-const MongoClient = require('monogdb').mongoClient;
-const ObjectId = require('mongodb').ObjectID;
-
-const assert = require('assert');
-
-const url = "mongodb//127:0.0.1:27017/test"
--- KimHeeSu_workspace/teamFile/myHTML/test.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".middle-right-1").innerHTML="";
-const element = document.createElement('p');
-element.textContent = "test";
-document.querySelector(".middle-right-1").appendChild(element);
-console.log("test")
